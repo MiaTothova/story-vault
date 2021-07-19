71165c1f960b0e62c66353d3664740fb40aa687b
MiaTothova <21tothova@gmail.com>
2021-07-19 10:21:25 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,1 +1,7 @@
-# python code goes here
+color = input("Enter color ")
+noun = input("Input a noun ")
+person = input("Enter a person ")
+
+print("Roses are " + color)
+print(noun + " are blue")
+print("I love " + person)
