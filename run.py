79aebf52a7f6c048ed99c0c2f4ec7733cb56e6d3
MiaTothova <21tothova@gmail.com>
2021-07-20 09:58:56 +0000

    Add Story templates

--- run.py
+++ run.py
@@ -1,7 +1,63 @@
-color = input("Enter color ")
-noun = input("Input a noun ")
-person = input("Enter a person ")
+def welcome():
+    print("Welcome to Story Vault :) \n")
+    user = input("Please enter your name : \n")
+    print(user)
 
-print("Roses are " + color)
-print(noun + " are blue")
-print("I love " + person)
+
+welcome()
+
+
+adjective = input("Insert a Adjective...\n")
+verb = input("Insert a Verb ...\n")
+noun = input("Insert a Noun ...\n")
+animal = input("Insert an Animal...\n ")
+vehicle = input("Insert something you ride in...\n ")
+color = input("Insert a colour ....\n ")
+foods = input("Insert Foods (plural) ....\n")
+person = input("Insert Person's Name ...\n ")
+phrase = input("Insert a Phrase ....\n ")
+
+
+def story_1():
+    print("Today I went to my favorite Taco Stand called ") 
+    print(f"the {adjective} {animal}. Unlike most food stands,")
+    print(f" they cook and prepare the food in a {vehicle}") 
+    print(f"while you {verb}. The best thing on the menu is ")
+    print(f"the {color} {noun}. Instead of ground beef they ")
+    print(f"fill the taco with {foods}, cheese, and top it off")
+    print(f"  with a salsa made from {foods}.") 
+    print("If that doesn't make your mouth water, ")
+    print(f"then it' just like {person} always says: {phrase}!")
+
+
+story_1()
+
+
+#story_2()
+ #   print("I just got back from a pizza party with{ PERSON}.")
+ #   print( "Can you believe we got to eat {ADJETIVE} pizza in A {PLACE}?!")
+ #   print("Everyone got to choose their own toppings. ")
+  #  print("I made '{FOOD} and {THINGS}' pizza, which is my favorite!")
+  #  print("They even stuffed the crust with {THINGS(p)}. How {FEELING}! ")
+  #  print("If that wasn't good enough already, A {CELEBRITY} was there singing {SONG TITLE}.")
+   # print("I was so inspired by the music, ")
+    
+
+#story_3()
+  #  print("Hi my name is {celebrity}, ")
+   # print("but my friends call me {silly word}")
+   # print("My favorite color is the {color} of {THINGS} and my favorite ")
+  #  print("thing to do is VERB. My parents were a {ANIMAL} and {JOB},)
+   # print("which is why we lived in {PLACE}. You probably know me from")
+   # print("my TV commercial for {SOMETHING YOU BUY}. ")
+   # print("I'm the one who says, '{SAYING}' at the very end! ")
+
+
+#story_4()
+    #print("Last night I dreamed I was a {ADJETIVE} butterfly")
+    #print("with {COLOR} splotches that looked like {THINGS}." )
+    #print("I flew to {PLACE} with my best friend, {PERSON}, ")
+   # print("who was a {ADJETIVE} INSECT. We ate some {FOOD} when")
+   # print("we got there and then decided to {VERB}. The dream")
+   # print("ended when I said, '{PHRASE}.' ")
+    
